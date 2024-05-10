5baf6c7c46f9f8cf0b9389db6a3c74f7a88af262
Michał Dyło <dylomichal@gmail.com>
2024-05-10 19:02:31 +0200

    Added player shooting mechanics

--- homiczuwka/Assets/Scripts/PlayerMovement.cs
+++ homiczuwka/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private float moveSpeed = 6f;
+    private float speedX, speedY;
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        speedX = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        speedY = Input.GetAxisRaw("Vertical") * moveSpeed;
+        rb.velocity = new Vector2(speedX, 0);
     }
 }
